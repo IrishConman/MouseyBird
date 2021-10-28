767a18d7f2ac1367ccaafe6bef9995ae3504bb19
Connor <56924624+IrishConman@users.noreply.github.com>
2021-10-28 11:58:57 -0500

    Implement movement
    Make the bird track the user's cursor

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-// This is not currently functional. Just a background
-
 const WIDTH = getWidth();
 const HEIGHT = getHeight();
 const CENTER_X = WIDTH / 2;
 const CENTER_Y = HEIGHT / 2;
+
+// Sloppy, but hey it works
+var birdObj = {
+    body: 0,
+    eye: 0,
+    pupil: 0,
+    beak: 0,
+    color: Randomizer.nextColor(),
+    rad: 20,
+    x: CENTER_X,
+    y: CENTER_Y
+};
 
 function makeBg() {
     var bgSky, bgGround;
@@ -26,48 +36,56 @@ function makeBird(birdObj) {
     birdObj.body.setColor(birdObj.color);
     
     const EYE_RAD = birdObj.rad / 3;
-    var EYE_X = birdObj.x + EYE_RAD * 2.5, EYE_Y = birdObj.y - EYE_RAD;
+    var eye_x = birdObj.x + EYE_RAD * 2.5, eye_y = birdObj.y - EYE_RAD;
     
     birdObj.eye = new Circle(EYE_RAD);
-    birdObj.eye.setPosition(EYE_X, EYE_Y);
+    birdObj.eye.setPosition(eye_x, eye_y);
     birdObj.eye.setColor(Color.white);
     
     const PUPIL_RAD = EYE_RAD / 2;
-    var PUPIL_X = EYE_X + PUPIL_RAD, PUPIL_Y = EYE_Y - PUPIL_RAD;
+    var pupil_x = eye_x + PUPIL_RAD, pupil_y = eye_y - PUPIL_RAD;
     
     birdObj.pupil = new Circle(PUPIL_RAD);
-    birdObj.pupil.setPosition(PUPIL_X, PUPIL_Y);
+    birdObj.pupil.setPosition(pupil_x, pupil_y);
     birdObj.pupil.setColor(Color.black);
     
     const BEAK_W = birdObj.rad  / 2, BEAK_H = birdObj.rad / 3;
-    var BEAK_X = birdObj.x + (birdObj.rad / 1.5), BEAK_Y = birdObj.y;
+    var beak_x = birdObj.x + (birdObj.rad / 1.5), beak_y = birdObj.y;
     
     birdObj.beak = new Rectangle(BEAK_W, BEAK_H);
-    birdObj.beak.setPosition(BEAK_X, BEAK_Y);
+    birdObj.beak.setPosition(beak_x, beak_y);
     birdObj.beak.setColor(Color.orange);
+}
+
+function moveBird(e) {
+    birdObj.x = e.getX();
+    birdObj.y = e.getY();
+    
+    const EYE_RAD = birdObj.rad / 3;
+    var eye_x = birdObj.x + EYE_RAD * 2.5, eye_y = birdObj.y - EYE_RAD;
+    const PUPIL_RAD = EYE_RAD / 2;
+    var pupil_x = eye_x + PUPIL_RAD, pupil_y = eye_y - PUPIL_RAD;
+    const BEAK_W = birdObj.rad  / 2, BEAK_H = birdObj.rad / 3;
+    var beak_x = birdObj.x + (birdObj.rad / 1.5), beak_y = birdObj.y;
+    
+    birdObj.body.setPosition(birdObj.x, birdObj.y);
+    birdObj.eye.setPosition(eye_x, eye_y);
+    birdObj.pupil.setPosition(pupil_x, pupil_y);
+    birdObj.beak.setPosition(beak_x, beak_y);
 }
 
 // Yes, this isn't C++. I'm still making a main function anyway
 function main() {
     makeBg();
     
-    var birdObj = {
-        body: 0,
-        eye: 0,
-        pupil: 0,
-        beak: 0,
-        color: Color.yellow,
-        rad: 20,
-        x: CENTER_X,
-        y: CENTER_Y
-    };
-    
     makeBird(birdObj);
     
     add(birdObj.body);
     add(birdObj.eye);
     add(birdObj.pupil);
-    add(birdObj.beak);   
+    add(birdObj.beak);
+    
+    mouseMoveMethod(moveBird);
 }
 
 main();
