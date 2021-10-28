9ccec7bc8dd48dca89c7b85a9129267ab2ec55a5
Connor <56924624+IrishConman@users.noreply.github.com>
2021-10-28 10:12:33 -0500

    Fix const vars
    Changed some constants into regular variables

--- main.js
+++ main.js
@@ -24,49 +24,50 @@ function makeBird(birdObj) {
     birdObj.body = new Circle(birdObj.rad);
     birdObj.body.setPosition(birdObj.x, birdObj.y);
     birdObj.body.setColor(birdObj.color);
-    // add(birdArr[BODY]);
     
     const EYE_RAD = birdObj.rad / 3;
-    const EYE_X = birdObj.x + EYE_RAD * 2.5, EYE_Y = birdObj.y - EYE_RAD;
+    var EYE_X = birdObj.x + EYE_RAD * 2.5, EYE_Y = birdObj.y - EYE_RAD;
     
     birdObj.eye = new Circle(EYE_RAD);
     birdObj.eye.setPosition(EYE_X, EYE_Y);
     birdObj.eye.setColor(Color.white);
-    // add(birdArr[EYE]);
     
     const PUPIL_RAD = EYE_RAD / 2;
-    const PUPIL_X = EYE_X + PUPIL_RAD, PUPIL_Y = EYE_Y - PUPIL_RAD;
+    var PUPIL_X = EYE_X + PUPIL_RAD, PUPIL_Y = EYE_Y - PUPIL_RAD;
     
     birdObj.pupil = new Circle(PUPIL_RAD);
     birdObj.pupil.setPosition(PUPIL_X, PUPIL_Y);
     birdObj.pupil.setColor(Color.black);
-    // add(birdArr[PUPIL]);
     
     const BEAK_W = birdObj.rad  / 2, BEAK_H = birdObj.rad / 3;
-    const BEAK_X = birdObj.x + (birdObj.rad / 1.5), BEAK_Y = birdObj.y;
+    var BEAK_X = birdObj.x + (birdObj.rad / 1.5), BEAK_Y = birdObj.y;
     
     birdObj.beak = new Rectangle(BEAK_W, BEAK_H);
     birdObj.beak.setPosition(BEAK_X, BEAK_Y);
     birdObj.beak.setColor(Color.orange);
-    // add(birdArr[BEAK]);
 }
 
-makeBg();
+// Yes, this isn't C++. I'm still making a main function anyway
+function main() {
+    makeBg();
+    
+    var birdObj = {
+        body: 0,
+        eye: 0,
+        pupil: 0,
+        beak: 0,
+        color: Color.yellow,
+        rad: 20,
+        x: CENTER_X,
+        y: CENTER_Y
+    };
+    
+    makeBird(birdObj);
+    
+    add(birdObj.body);
+    add(birdObj.eye);
+    add(birdObj.pupil);
+    add(birdObj.beak);   
+}
 
-var birdObj = {
-    body: 0,
-    eye: 0,
-    pupil: 0,
-    beak: 0,
-    color: Color.yellow,
-    rad: 20,
-    x: CENTER_X,
-    y: CENTER_Y
-};
-
-makeBird(birdObj);
-
-add(birdObj.body);
-add(birdObj.eye);
-add(birdObj.pupil);
-add(birdObj.beak);
+main();
